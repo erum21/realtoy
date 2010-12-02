97078d8dc41c7d19218b15c3d83f94d7dd308991
Edwin Vega <edwin.ponc@gmail.com>
2010-12-02 17:08:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Realtoy::Application.config.secret_token = '208d3302b4deb43109c68162390970173b316a21ed907e37504ae700b13624c0611646124e50b428145753f99e8b1f755b185e2f85628df3d518749941f29e68'
